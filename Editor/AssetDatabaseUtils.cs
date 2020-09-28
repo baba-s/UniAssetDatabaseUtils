9eb1ac3ce4cdbae3cfb8569b116fbb4b75188941
baba_s <shotababa1206@gmail.com>
2020-09-28 19:40:22 +0900

    first commit

---
+++ Editor/AssetDatabaseUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Kogane
+{
+	public static class AssetDatabaseUtils
+	{
+		public static T FindAsset<T>() where T : UnityEngine.Object
+		{
+			var guids = AssetDatabase.FindAssets( $"t:{typeof( T ).Name}" );
+			var guid  = guids.FirstOrDefault();
+			var path  = AssetDatabase.GUIDToAssetPath( guid );
+			var asset = AssetDatabase.LoadAssetAtPath<T>( path );
+
+			return asset;
+		}
+
+		public static T[] FindAssets<T>() where T : UnityEngine.Object
+		{
+			return AssetDatabase
+					.FindAssets( $"t:{typeof( T ).Name}" )
+					.Select( c => AssetDatabase.GUIDToAssetPath( c ) )
+					.Select( c => AssetDatabase.LoadAssetAtPath<T>( c ) )
+					.ToArray()
+				;
+		}
+	}
+}
